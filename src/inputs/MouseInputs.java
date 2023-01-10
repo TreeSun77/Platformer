5d351a128a76eb4ab56c21df15ed34e0431146bc
TreeSun77 <TreeSun77@TreeSun77.ru>
2023-01-10 03:07:43 +0300

    Game loop + FPS 120

--- src/inputs/MouseInputs.java
+++ src/inputs/MouseInputs.java
@@ -49,7 +49,7 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		gamePanel.setRectPos(e.getX(), e.getY());
+	//	gamePanel.setRectPos(e.getX(), e.getY());
 
 	}
 }
